d484137298fee0a5e89b458b5d84f7a63fe100f9
Henrik Farre <henrik@rockhopper.dk>
2021-11-30 19:35:20 +0100

    Indent is sometimes broken

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -6,9 +6,9 @@ require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
   },
-  indent = {
-    enable = true,
-  },
+  -- indent = {
+  --   enable = true,
+  -- },
   incremental_selection = {
     enable = true,
   },
