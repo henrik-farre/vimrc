5493b4f1e6f06ca0dafcbd56db295f71afc729a7
Henrik Farre <henrik@rockhopper.dk>
2022-09-16 14:51:36 +0200

    Testing mypy

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -15,6 +15,7 @@ if vim.g.plugs['null-ls.nvim'] then
     sources = {
       require("null-ls").builtins.diagnostics.flake8,
       require("null-ls").builtins.diagnostics.shellcheck,
+      require("null-ls").builtins.diagnostics.mypy,
     },
   })
 end
