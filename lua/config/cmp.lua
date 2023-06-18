a656994921f245dad3bccf64138f615f396ddf84
Henrik Farre <henrik@rockhopper.dk>
2023-06-18 13:41:35 +0200

    Remove unused mappings

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -43,9 +43,6 @@ if vim.g.plugs['nvim-cmp'] then
       documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
       ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
