734f415c914653c0da5712fb653814c1f7faed01
Henrik Farre <henrik@rockhopper.dk>
2024-01-05 15:16:22 +0100

    Tweak neo-tree config to better track the current displayed file

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -38,14 +38,35 @@ end
 
 if vim.g.plugs['neo-tree.nvim'] then
   require('neo-tree').setup{
+    default_component_configs = {
+      name = {
+        trailing_slash = false,
+        use_git_status_colors = true,
+        highlight = "NeoTreeFileName",
+      },
+    },
     filesystem = {
+      bind_to_cwd = true, -- true creates a 2-way binding between vim's cwd and neo-tree's root
+      cwd_target = {
+        sidebar = "tab",   -- sidebar is when position = left or right
+        current = "window" -- current is when position = current
+      },
       follow_current_file = {
         enabled = true, -- This will find and focus the file in the active buffer every
+        --              -- the current file is changed while the tree is open.
+        leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
       },
       filtered_items = {
         hide_dotfiles = false,
         hide_gitignored = false,
       },
+    },
+    buffers = {
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every time
+        --              -- the current file is changed while the tree is open.
+        leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      }
     }
   }
 end
