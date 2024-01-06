6426c3a502f1e27fcfb7a8a7b36e26b5f0a3ae47
Henrik Farre <henrik@rockhopper.dk>
2024-01-06 14:26:15 +0100

    Cleanup duplicate, match other syntax for require

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -5,7 +5,7 @@ if vim.loop.os_uname().machine == "aarch64" then
   return
 end
 
-require'nvim-treesitter.configs'.setup {
+require('nvim-treesitter.configs').setup {
   ensure_installed = { "dockerfile", "yaml", "python", "bash", "json", "javascript", "html", "css", "lua", "markdown", "toml", "hcl"},
   highlight = {
     enable = true,
@@ -14,9 +14,6 @@ require'nvim-treesitter.configs'.setup {
     enable = false,
   },
   incremental_selection = {
-    enable = true,
-  },
-  matchup = {
     enable = true,
   },
   -- https://github.com/andymass/vim-matchup
