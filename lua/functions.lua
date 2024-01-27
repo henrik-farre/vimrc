f19621641a820bd89f510893e62daa466bab4d7a
Henrik Farre <henrik@rockhopper.dk>
2024-01-27 11:52:35 +0100

    Move ReplaceEscapedChars to lua

--- lua/functions.lua
+++ lua/functions.lua
@@ -1,4 +1,4 @@
-------------------------------------------------------------
+-- -----------------------------------------------------------------------------
 -- Output selected yaml schema from yaml-companion
 --
 function get_yaml_schema()
@@ -8,3 +8,14 @@ function get_yaml_schema()
   end
   return ""
 end
+
+-- -----------------------------------------------------------------------------
+-- Replaces escaped chars like \n and \t with the real ones
+--
+function ReplaceEscapedChars()
+  vim.cmd([[
+    %s/\\n/\r/
+    %s/\\t/\t/
+  ]])
+end
+vim.api.nvim_create_user_command('ReplaceEscapedChars', function() ReplaceEscapedChars() end, { desc = 'Replace \n and \t with real chars' })
