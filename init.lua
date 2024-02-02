66ee29592b607622068f5cf5af748d76ce1c2920
Henrik Farre <henrik@rockhopper.dk>
2024-02-02 22:51:37 +0100

    Tweak wildignore

--- init.lua
+++ init.lua
@@ -73,17 +73,20 @@ vim.o.showfulltag = true    -- Show full tag for completion
 -- Wild ignore/mode and suffixes
 --
 -- Based on https://bitbucket.org/sjl/dotfiles/src/141b96496989091fce4aa5165946f94d31c2374f/vim/vimrc
-vim.opt.wildignore:append(".hg,.git,.svn,CSV")                    -- Version control
-vim.opt.wildignore:append("*.aux,*.out,*.toc")                    -- LaTeX intermediate files
-vim.opt.wildignore:append("*.jpg,*.bmp,*.gif,*.png,*.jpeg,*.pdf") -- binary images
-vim.opt.wildignore:append("*.o,*.obj,*.exe,*.dll,*.manifest")     -- compiled object files
-vim.opt.wildignore:append("*.spl")                                -- compiled spelling word lists
-vim.opt.wildignore:append("*.sw?")                                -- Vim swap files
-vim.opt.wildignore:append("*.DS_Store")                           -- OSX bullshit
-vim.opt.wildignore:append("*.luac")                               -- Lua byte code
-vim.opt.wildignore:append("*.pyc")                                -- Python byte code
-vim.opt.wildignore:append("*.orig")                               -- Merge resolution files
-vim.opt.wildignore:append("*.zip,*.tgz")                          -- Compressed files
+-- ignored by cmp cmdline :(
+vim.opt.wildignore:append({
+  ".hg,.git,.svn,CSV",                    -- Version control
+  "*.aux,*.out,*.toc",                    -- LaTeX intermediate files
+  "*.jpg,*.bmp,*.gif,*.png,*.jpeg,*.pdf", -- binary images
+  "*.o,*.obj,*.exe,*.dll,*.manifest",     -- compiled object files
+  "*.spl",                                -- compiled spelling word lists
+  "*.sw?",                                -- Vim swap files
+  "*.DS_Store",                           -- OSX bullshit
+  "*.luac",                               -- Lua byte code
+  "*.pyc",                                -- Python byte code
+  "*.orig",                               -- Merge resolution files
+  "*.zip,*.tgz",                          -- Compressed files
+})
 
 vim.o.wildmode = "list:longest,full"
 vim.o.wildignorecase = true
