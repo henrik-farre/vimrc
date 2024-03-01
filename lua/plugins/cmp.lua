b75fb906d5def30974eaf59e82207045aa361062
Henrik Farre <henrik@rockhopper.dk>
2024-03-01 16:33:55 +0100

    Just select on enter

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -102,7 +102,17 @@ return {
 
     -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
     cmp.setup.cmdline(':', {
-      mapping = cmp.mapping.preset.cmdline(),
+      mapping = cmp.mapping.preset.cmdline({
+        ['<CR>'] = {
+          c = function(default)
+            if cmp.visible() then
+              return cmp.confirm({ select = true })
+            end
+
+            default()
+          end,
+        },
+      }),
       sources = cmp.config.sources({
         { name = 'async_path' }
       }, {
