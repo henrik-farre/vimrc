c29a6153676f1a0342c9f9718478519d7bc880c1
Henrik Farre <henrik@rockhopper.dk>
2024-03-08 14:11:44 +0100

    Revert "Just select on enter"
    This reverts commit b75fb906d5def30974eaf59e82207045aa361062.

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -102,17 +102,7 @@ return {
 
     -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
     cmp.setup.cmdline(':', {
-      mapping = cmp.mapping.preset.cmdline({
-        ['<CR>'] = {
-          c = function(default)
-            if cmp.visible() then
-              return cmp.confirm({ select = true })
-            end
-
-            default()
-          end,
-        },
-      }),
+      mapping = cmp.mapping.preset.cmdline(),
       sources = cmp.config.sources({
         { name = 'async_path' }
       }, {
