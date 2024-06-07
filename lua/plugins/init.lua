b6b403b88e904e44485d7c23c4ae9faaf69c7528
Henrik Farre <henrik@rockhopper.dk>
2024-06-07 15:11:00 +0200

    Enable keymaps for incremental search

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -23,6 +23,12 @@ return {
         },
         incremental_selection = {
           enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
         },
         -- https://github.com/andymass/vim-matchup
         matchup = {
