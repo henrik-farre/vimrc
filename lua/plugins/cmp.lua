876cea2d70ab309cf2787c24ec6d3b96315be0db
Henrik Farre <henrik@rockhopper.dk>
2025-04-14 20:58:21 +0200

    Fix whitespace

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -85,7 +85,7 @@ return {
     -- Set configuration for specific filetype.
     cmp.setup.filetype('gitcommit', {
       sources = cmp.config.sources({
-        { name = 'cmp_git' },   -- You can specify the `cmp_git` source if you were installed it.
+        { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
       }, {
         { name = 'buffer' },
       })
